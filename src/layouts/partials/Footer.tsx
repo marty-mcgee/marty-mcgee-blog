8a01b05b76060727e83f402251a802b03931aecb
Marty McGee <support@companyjuice.com>
2024-10-07 16:17:35 -0700

    🐞 v3.0.4: app: content

--- src/layouts/partials/Footer.tsx
+++ src/layouts/partials/Footer.tsx
@@ -23,7 +23,8 @@ const Footer = ({
       <div className="container">
         <div className="row items-center py-10">
           <div className="mb-8 text-center lg:col-3 lg:mb-0 lg:text-left">
-            <Logo lang={lang} />
+            {/* <Logo lang={lang} /> */}
+            <p dangerouslySetInnerHTML={markdownify(copyright)} />
           </div>
           <div className="mb-8 text-center lg:col-6 lg:mb-0">
             <ul>
@@ -39,11 +40,11 @@ const Footer = ({
           </div>
         </div>
       </div>
-      <div className="border-t border-border py-7 dark:border-darkmode-border">
+      {/* <div className="border-t border-border py-7 dark:border-darkmode-border">
         <div className="container text-center text-light dark:text-darkmode-light">
           <p dangerouslySetInnerHTML={markdownify(copyright)} />
         </div>
-      </div>
+      </div> */}
     </footer>
   );
 };
